05c11a0ccc7a7464e53b2f21d21ad3115c08ee8c
Yannick Marcon <yannick.marcon@obiba.org>
2018-04-18 15:56:22 +0200

    Variable categories (name, missing, label) added

--- R/mica.variables.R
+++ R/mica.variables.R
@@ -21,7 +21,8 @@
 #' @param df Return a data.frame (default is TRUE)
 #' @export
 mica.variables <- function(mica, query="variable()",  
-                           select=list("attributes","nature","valueType"), sort=list("id"), 
+                           select=list("*"), 
+                           sort=list("id"), 
                            from=0, limit=10000, locale="en", df=TRUE) {
   q <- .append.rql(query, "variable", select, sort, from, limit, locale)
   res <- .post(mica, "variables", "_rql", query=list(query=q))
@@ -37,8 +38,18 @@ mica.variables <- function(mica, query="variable()",
     nature <- rep(NA, length(summaries))
     datasetId <- rep(NA, length(summaries))
     studyId <- rep(NA, length(summaries))
+    populationId <- rep(NA, length(summaries))
+    dceId <- rep(NA, length(summaries))
     variableType <- rep(NA, length(summaries))
+    mimeType <- rep(NA, length(summaries))
+    unit <- rep(NA, length(summaries))
+    referencedEntityType <- rep(NA, length(summaries))
+    repeatable <- rep(NA, length(summaries))
+    occurrenceGroup <- rep(NA, length(summaries))
     label <- rep(NA, length(summaries))
+    categories <- rep(NA, length(summaries))
+    categories.missing <- rep(NA, length(summaries))
+    categories.label <- rep(NA, length(summaries))
     annotations <- list()
     for(i in 1:length(summaries)) {
       v <- summaries[[i]]
@@ -48,8 +59,27 @@ mica.variables <- function(mica, query="variable()",
       nature[i] <- .nullToNA(v[["nature"]])
       datasetId[i] <- v[["datasetId"]]
       studyId[i] <- v[["studyId"]]
+      populationId[i] <- .nullToNA(v[["populationId"]])
+      dceId[i] <- .nullToNA(v[["dceId"]])
       variableType[i] <- v[["variableType"]]
+      mimeType[i] <- .nullToNA(v[["mimeType"]])
+      unit[i] <- .nullToNA(v[["unit"]])
+      referencedEntityType[i] <- .nullToNA(v[["referencedEntityType"]])
+      repeatable[i] <- .nullToNA(v[["repeatable"]])
+      occurrenceGroup[i] <- .nullToNA(v[["occurrenceGroup"]])
       label[i] <- .extractLabel(locale, v[["variableLabel"]])
+      if ("categories" %in% names(v)) {
+        categories[i] <- paste(lapply(v$categories, function(cat) { cat$name }), collapse = "|")
+        categories.missing[i] <- paste(lapply(v$categories, function(cat) { cat$missing }), collapse = "|")
+        categories.label[i] <- paste(lapply(v$categories, function(cat) { 
+          labels <- cat$attributes[lapply(cat$attributes, function(attr) { attr$name }) == "label"]
+          if (length(labels)>0) {
+            .extractLabel(locale, labels[[1]]$values)
+          } else {
+            ""
+          }
+          }), collapse = "|")
+      }
       if ("annotations" %in% names(v)) {
         for (annot in  v[["annotations"]]) {
           key <- paste0(annot$taxonomy, ".", annot$vocabulary)
@@ -62,15 +92,13 @@ mica.variables <- function(mica, query="variable()",
         }
       }
     }
-    df <- data.frame(id, name, valueType, nature, variableType, label, datasetId, studyId)
-    if (all(is.na(df$label))) {
-      df$label <- NULL
-    }
-    if (all(is.na(df$valueType))) {
-      df$valueType <- NULL
-    }
-    if (all(is.na(df$nature))) {
-      df$nature <- NULL
+    df <- data.frame(id, name, valueType, nature, categories, categories.missing, categories.label, 
+                     variableType, label, datasetId, studyId, populationId, dceId, 
+                     mimeType, unit, referencedEntityType, repeatable, occurrenceGroup)
+    for (key in c("label", "categories", "categories.missing", "categories.label", "populationId", "dceId", "valueType", "nature", "mimeType", "unit", "referencedEntityType", "repeatable", "occurrenceGroup")) {
+      if (all(is.na(df[key]))) {
+        df[key] <- NULL
+      }
     }
     for (col in names(annotations)) {
       df[[col]] <- annotations[[col]]
