8d5c97368c329e6908f29952257d7c42536810b5
Yannick Marcon <yannick.marcon@obiba.org>
2019-01-24 17:40:11 +0100

    DAR example

--- inst/examples/mica-dar.R
+++ inst/examples/mica-dar.R
@@ -1,7 +1,9 @@
 # Load library
 library(micar)
 
-# Get data access requests schema form and list (not open to 'anonymous' user)
+m <- mica.login(username="administrator", password="password", url="https://mica-demo.obiba.org")
+
+# Get data access requests schema form, list and a specific one
 mica.dar.form(m)
 mica.dars(m)
 mica.dar(m, id="390463")
